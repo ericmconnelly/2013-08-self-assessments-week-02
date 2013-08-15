792c0cc6fbdecd764e81fd6c85671a5b3cea1512
Fred Zirdung <fred@hackreactor.com>
2013-08-15 16:37:22 -0700

    Add D3 to self-assessments

---
+++ d3/d3_sample.js
@@ -0,0 +1,14 @@
+var width = 960,
+    height = 2200;
+
+var cluster = d3.layout.cluster()
+    .size([height, width - 160]);
+
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(40,0)");
